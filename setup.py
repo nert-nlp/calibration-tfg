690cbf581720641f01b4a6dd02213b0a6bc29740
Michael Kranzlein <>
2021-10-13 02:00:27 -0400

    Fixed package structure

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="calibration_tfg",
-      packages=["src/calibration_tfg"],
+setup(name="calibrationtfg",
+      packages=find_packages(),
+      package_dir={"": "src"},
       description="Runs tag frequency grouping calibration experiments",
       author="Michael Kranzlein",
-      version="1.0.0,"
+      version="1.0.0",
       project_urls={
           "github": "https://github.com/nert-nlp/calibration_tfg"
       })
---
+++ src/calibrationtfg/__init__.py
@@ -0,0 +1,6 @@
+from . import calibrate
+from . import evaluate
+from . import grouping
+from . import loader
+from . import main
+from . import util
